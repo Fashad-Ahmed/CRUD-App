febc78daf8aafdcc86c93026d4cca4cfe835325b
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2021-04-18 15:57:53 +0500

    complete one time commit 1.0

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+// creating connection
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "password",
+    database: "empsys",
+});
+
+app.post('/create', (req, res) => {
+    const name      = req.body.name;
+    const age       = req.body.age;
+    const country   = req.body.country;
+    const position  = req.body.position;
+    const salary    = req.body.salary;
+
+    db.query(
+        'INSERT INTO employee (name, age, country, position, salary) VALUES (?,?,?,?,?)',
+        [name, age, country, position, salary],
+        (err, res) => {
+            if(err) {
+                console.log(err);
+            }
+            else {
+                res.send('values INSERTED !!!');
+            }
+        }
+    );
+});
+
+app.listen(3001, () => {
+    console.log('running!')
+})
